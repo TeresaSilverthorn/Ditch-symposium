228648b5c88125e83e92ba1b0e9effdda1c3f8c0
TeresaSilverthorn <teresa.silverthorn@gmail.com>
2025-08-15 13:32:03 +0100

    GHG contribution plot

--- WOS_publicaton_data.R
+++ WOS_publicaton_data.R
@@ -6,6 +6,7 @@ library(readr)
 library(dplyr)
 library(ggplot2)
 library(data.table)
+library(ggpubr)
 
 # Load your data (modify filenames and column names as needed)
 ditch <- fread("C:/Users/teres/Documents/Conferences/Ditch symposium/Ditch symposium output data/ditch_canal_data_table.txt")  %>%
@@ -38,14 +39,14 @@ dat_ratio <- dat %>%
 
 jpeg("Publications_by_year.jpg", units="in", width=8, height=6, res=300)
 
-plot <- ggplot(subset(dat, `Publication Years` >= 1990 & `Publication Years` <= 2024 ), aes(x = `Publication Years`, y = `Record Count`, fill = factor(term, levels = c( "Stream/River", "Lake", "Ditch" ) )))  +
+pub_plot <- ggplot(subset(dat, `Publication Years` >= 1990 & `Publication Years` <= 2024 ), aes(x = `Publication Years`, y = `Record Count`, fill = factor(term, levels = c( "Stream/River", "Lake", "Ditch" ) )))  +
   geom_col(color = "black") +
-  labs( y = "# of Publications" ) +
+  labs( y = "# of publications" ) +
    scale_x_continuous(breaks = seq(1990, 2025, by = 5), expand = c(0, 0)) +
   scale_y_continuous(expand = c(0, 0)) +
   scale_fill_manual(values = c( "Stream/River"=  "#00798c", "Lake"= "#edae49", "Ditch" = "#ff6f61" )) +   #d1495b  "#f17c67",
-  theme_minimal() + theme(legend.position = "top",  axis.title.x = element_blank(), legend.title = element_blank(), axis.ticks = element_line(color = "black"), panel.grid = element_blank(), axis.line = element_line(color = "black"), axis.text.y = element_text(size=14, colour="black"),  axis.text.x =  element_text(size=14,  colour="black"), axis.title = element_text(size = 16,  colour="black"), legend.text = element_text(size = 14), panel.grid.major.y = element_line(color = "gray80", linewidth  = 0.5)  )
-plot
+  theme_minimal() + theme(legend.position = "top",  axis.title.x = element_blank(), legend.title = element_blank(), axis.ticks = element_line(color = "black"), panel.grid = element_blank(), axis.line = element_line(color = "black"), axis.text.y = element_text(size=14, colour="black"),  axis.text.x =  element_text(size=14,  colour="black"), axis.title = element_text(size = 18,  colour="black"), legend.text = element_text(size = 18), panel.grid.major.y = element_line(color = "gray80", linewidth  = 0.5)  )
+pub_plot
 
 dev.off()
 
@@ -88,5 +89,50 @@ ratio_plot_bar <- ggplot(subset(dat_ratio, `Publication Years` >= 1990 & `Public
   theme_minimal() + theme(legend.position = "top", axis.title.x = element_blank(), legend.title = element_blank(), axis.ticks = element_line(color = "black"), panel.grid = element_blank(), axis.line = element_line(color = "black"), axis.text.y = element_text(size=14, colour="black"),  axis.text.x =  element_text(size=14,  colour="black"), axis.title = element_text(size = 16,  colour="black"), legend.text = element_text(size = 14), panel.grid.major.y = element_line(color = "gray80", linewidth  = 0.5) ) +
   scale_y_continuous(breaks = seq(0, 4, by = 1))
 ratio_plot_bar
+
+dev.off()
+
+
+# GHG pie chart of contributions in GHG equivalents
+
+# Data from GCB article, based on 22 studies that measured all 3 gases CH4 is ebul+diff
+emissions <- data.frame(
+  gas = c("CO2", "CH4", "N2O"),
+  percentage = c(51, 43.5, 5.5))
+
+# Create pie chart
+pie <- ggplot(emissions, aes(x = "", y = percentage, fill = gas)) +
+  geom_col(width = 1, color = "white") +
+  coord_polar(theta = "y") +
+  geom_text(aes(label = paste0(percentage, "%")),
+            position = position_stack(vjust = 0.5)) +
+  scale_fill_manual(values = c("CO2" = "#edae49", "CH4" = "#00798c", "N2O" = "#ff6f61")) +
+  theme_void()
+pie
+
+
+jpeg("Contribution_by_gas.jpg", units="in", width=6, height=6, res=300)
+
+bar_gas <- ggplot(emissions, aes(x = reorder(gas, -percentage), y = percentage, fill = gas)) +
+  geom_col(color = "black") +
+  geom_text(aes(label = paste0(percentage, "%")),
+            vjust = -0.5, size = 5) +
+  scale_fill_manual(values = c("CO2" = "#edae49", "CH4" = "#00798c", "N2O" = "#ff6f61")) +
+  scale_x_discrete(labels = c(expression(CO[2]), expression(CH[4]), expression(N[2]*O))) +
+  labs( y = "% contribution to ditch emissions") +
+  scale_y_continuous(expand = expansion(mult = 0), limits = c(0, 60))+
+  theme_minimal() +
+  theme(legend.position = "none", axis.title.x = element_blank(), legend.title = element_blank(), axis.ticks = element_line(color = "black"), panel.grid = element_blank(), axis.line = element_line(color = "black"), axis.text.y = element_text(size=14, colour="black"),  axis.text.x =  element_text(size=14,  colour="black"), axis.title = element_text(size = 18,  colour="black"), legend.text = element_text(size = 14) ) 
+bar_gas
+
+dev.off()
+
+# combine
+
+jpeg("Combined.jpg", units="in", width=12, height=5, res=300)
+
+combine1 <- ggarrange(pub_plot, bar_gas, 
+                      nrow = 1, ncol = 2, align = "v", widths = c( 1.5, 1))  
+combine1
 
 dev.off()
